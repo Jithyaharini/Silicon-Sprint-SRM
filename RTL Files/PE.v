77e78acd41f5c9308c203f25c857f2246fee0512
Jithyaharini <jithyaharini2310725@ssn.edu.in>
2026-03-11 15:27:53 +0530

    PE.v

--- RTL Files/PE.v
+++ RTL Files/PE.v
@@ -20,28 +20,49 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 // Low-power Processing Element with clock gating for zero detection
+`timescale 1ns / 1ps
+// Low-power Processing Element with clock gating for zero detection
 module PE(
-    input clk,
-    input rst,
-    input [7:0] a,
-    input [7:0] b,
-    input [31:0] sum_in,
-    output reg [31:0] sum_out
+input clk,
+input rst,
+
+input [7:0] a0,a1,a2,a3,
+input [7:0] b0,b1,b2,b3,
+
+output reg [31:0] sum
 );
-wire compute_enable;
 
-assign compute_enable = (a != 0) && (b != 0);
+reg [31:0] temp;
 
-always @(posedge clk or posedge rst) begin
-    if(rst)
-        sum_out <= 0;
+always @(*) begin
+    temp = 0;
 
-    else if(compute_enable)
-        sum_out <= sum_in + (a * b);
+    // Zero-sparsity checks
+    if(a0 != 0 && b0 != 0)
+        temp = temp + a0*b0;
 
-    else
-        sum_out <= sum_in;
+    if(a1 != 0 && b1 != 0)
+        temp = temp + a1*b1;
+
+    if(a2 != 0 && b2 != 0)
+        temp = temp + a2*b2;
+
+    if(a3 != 0 && b3 != 0)
+        temp = temp + a3*b3;
 end
+
+
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        sum <= 0;
+    else
+        sum <= temp;
+end
+
 endmodule
+
+
+
 
 
