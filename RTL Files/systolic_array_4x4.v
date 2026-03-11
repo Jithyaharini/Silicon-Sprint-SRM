c3aa18269668054c767b04e9ce263b81acbe32e8
Jithyaharini <jithyaharini2310725@ssn.edu.in>
2026-03-11 15:31:27 +0530

    systolic_array_4x4.v

--- RTL Files/systolic_array_4x4.v
+++ RTL Files/systolic_array_4x4.v
@@ -1,4 +1,24 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.03.2026 12:45:47
+// Design Name: 
+// Module Name: PE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module systolic_array_4x4(
     input clk,
     input rst,
